9028528b33527102f9ed2611c0c44f3950f8a2ec
Jan Sterba <info@jansterba.com>
2023-01-31 11:21:46 +0100

    fix org search

--- lib/pipedrive/organization.rb
+++ lib/pipedrive/organization.rb
@@ -8,13 +8,14 @@ module Pipedrive
     include ::Pipedrive::Operations::Delete
     include ::Pipedrive::Utils
 
-    def find_by_name(*args, &block)
+    def search(*args, &block)
       params = args.extract_options!
       params[:term] ||= args[0]
       raise 'term is missing' unless params[:term]
-      return to_enum(:find_by_name, params) unless block
 
-      follow_pagination(:make_api_call, [:get, 'find'], params, &block)
+      return to_enum(:search, params) unless block
+
+      follow_pagination(:make_api_call, [:get, 'search'], params, &block)
     end
   end
 end
